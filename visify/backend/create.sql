c0664e3433461d3b99ba7c75f43fbcefc7795124
Andrew Strong <strong.andrew.j@gmail.com>
2023-03-29 23:14:04 -0600

    pushing

--- visify/backend/create.sql
+++ visify/backend/create.sql
@@ -3,5 +3,5 @@ CREATE TABLE IF NOT EXISTS users (
   fullname varchar(70) NOT NULL,
   password varchar(255) NOT NULL,
   email varchar(255) NOT NULL,
-  PRIMARY KEY(id) /* A game's unique primary key consists of the visitor_name & the game date (this assumes you can't have multiple games against the same team in a single day) */
+  PRIMARY KEY(id)
 );
